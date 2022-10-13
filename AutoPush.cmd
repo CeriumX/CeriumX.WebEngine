5b9739cd770f2fa02543acabcdd47cacfff9adb6
CockroachSoulful <cockroach888@outlook.com>
2022-10-13 11:32:49 +0800

    feat: 增加多个远程仓库自动推送脚本。

---
+++ AutoPush.cmd
@@ -0,0 +1,28 @@
+@echo off
+@title GIT 自动推送到远程仓库
+
+set basedir="%~dp0"
+set basedir
+
+
+@echo\&echo 推送到 Gitee 远程仓库
+git push gitee
+
+@echo\&echo 推送到 GitHub 远程仓库
+git push github
+
+@echo\&echo 推送到 Origin 远程仓库
+git push origin
+
+@echo\&echo 查看本地仓库状态
+git status
+
+
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo\&echo 完成所有远程仓库推送，请按任意键退出。
+pause>nul 
+exit
