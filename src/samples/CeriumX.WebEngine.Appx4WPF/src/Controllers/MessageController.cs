82dcc223149c5e2aae51d2413233bf14448ab25a
CockroachSoulful <cockroach888@outlook.com>
2022-10-14 11:23:12 +0800

    feat: 完成基于WebView2的功能实现示例演示编码工作。

---
+++ src/samples/CeriumX.WebEngine.Appx4WPF/src/Controllers/MessageController.cs
@@ -0,0 +1,72 @@
+﻿//=========================================================================
+//**   Web引擎服务产品中间件（CeriumX.WebEngine）
+//=========================================================================
+//**   脉脉含情的充满精神的高尚的小强精神
+//**   风幽思静繁花落；夜半楼台听江雨。（cockroach888@outlook.com）
+//=========================================================================
+//**   Copyright © 蟑螂·魂 2022 -- Support 华夏银河空间联盟
+//=========================================================================
+// 文件名称：MessageController.cs
+// 项目名称：Web引擎服务产品中间件
+// 创建时间：2022-10-14 10:09:08
+// 创建人员：宋杰军
+// 电子邮件：cockroach888@outlook.com
+// 负责人员：宋杰军
+// 参与人员：宋杰军
+// ========================================================================
+// 修改日期：
+// 修改人员：
+// 修改内容：
+// ========================================================================
+using System.Runtime.InteropServices;
+using System.Windows;
+
+namespace CeriumX.WebEngine.Appx4WPF.Controllers;
+
+/// <summary>
+/// 消息控制器
+/// </summary>
+/// <remarks>
+/// <para>await chrome.webview.hostObjects.message.ShowMsg('This is haha.')</para>
+/// <para>await chrome.webview.hostObjects.message.Prop</para>
+/// </remarks>
+#pragma warning disable CS0618 // 类型或成员已过时
+[ClassInterface(ClassInterfaceType.AutoDual)]
+#pragma warning restore CS0618 // 类型或成员已过时
+[ComVisible(true)]
+public sealed class MessageController : IWebController
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public string Prop { get; set; } = "Example";
+
+
+    /// <summary>
+    /// 显示信息
+    /// </summary>
+    /// <param name="msg">需要显示的信息</param>
+    public void ShowMsg(string msg)
+    {
+        MessageBox.Show(msg);
+    }
+
+
+    #region 接口实现[IWebController]
+
+    /// <summary>
+    /// 将CSharp对象注册为JS对象的控制器名称
+    /// </summary>
+    public string ControllerName { get; } = "message";
+
+    /// <summary>
+    /// 资源释放
+    /// </summary>
+    public void Dispose()
+    {
+        // do something.
+    }
+
+    #endregion
+
+}
--- src/samples/CeriumX.WebEngine.Appx4WPF/src/MainWindow.xaml.cs
+++ src/samples/CeriumX.WebEngine.Appx4WPF/src/MainWindow.xaml.cs
@@ -13,16 +13,118 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace CeriumX.WebEngine.Appx4WPF
+namespace CeriumX.WebEngine.Appx4WPF;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
+    private IWebWindowFactory? _webFactory;
+    private IWebWindow<UIElement>? _webWindow;
+
+
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// 
     /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+        // do something.
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="e"></param>
+    protected override async void OnInitialized(EventArgs e)
+    {
+        base.OnInitialized(e);
+
+        try
         {
-            InitializeComponent();
+            string webViewFolder = System.IO.Path.GetFullPath("d:/data/WebView2Fixed");
+            string userDataFolder = System.IO.Path.GetFullPath("d:/data/WebView2Fixed/UserData");
+
+            _webFactory = WebWindowFactoryProvider.CreateWebWindowFactory(
+                browserExecutableFolder: webViewFolder,
+                userDataFolder: userDataFolder,
+                nlogRulePrefixName: "Monica");
+            await _webFactory.InitializeEnvironmentAsync().ConfigureAwait(false);
+
+            WebOptions options = WebOptions.Create(@"http://localhost");
+            _webWindow = await _webFactory.CreateAsync<UIElement>(options).ConfigureAwait(false);
+            mainContent.Child = _webWindow.BrowserControl;
+
+            _webWindow.OnWebBrowserInitializationCompleted += (sender, e) =>
+            {
+                if (e.IsSuccess)
+                {
+                    _webWindow?.Browser?.AddControllerToScript(new MessageController());
+                }
+            };
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message, "系统提示", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+
+    private void BtnTest_Click(object sender, RoutedEventArgs e)
+    {
+        if (_webWindow != null && _webWindow.Browser != null)
+        {
+            _webWindow?.Browser?.Navigate(txtAddress.Text.Trim());
+        }
+    }
+
+    private void GoBack_Click(object target, ExecutedRoutedEventArgs e)
+    {
+        _webWindow?.Browser?.GoBack();
+    }
+
+    private void GoForward_Click(object target, ExecutedRoutedEventArgs e)
+    {
+        _webWindow?.Browser?.GoForward();
+    }
+
+    private void BtnTaskMgr_Click(object sender, RoutedEventArgs e)
+    {
+        _webWindow?.Browser?.OpenTaskManagerWindow();
+    }
+
+    private void BtnDevTool_Click(object sender, RoutedEventArgs e)
+    {
+        _webWindow?.Browser?.OpenDevToolsWindow();
+    }
+
+    private void BtnConsoleLog_Click(object sender, RoutedEventArgs e)
+    {
+        if (_webWindow != null && _webWindow.Browser != null)
+        {
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    string result = await _webWindow.Browser.ExecuteScriptAsync($"console.log({i})");
+            //    Console.WriteLine(result);
+            //}
+
+            Parallel.For(0, 10, (i) =>
+            {
+                Dispatcher.BeginInvoke(async () =>
+                {
+                    string result = await _webWindow.Browser.ExecuteScriptAsync($"console.log({i})").ConfigureAwait(false);
+                    Console.WriteLine(result);
+                });
+            });
         }
     }
 }
