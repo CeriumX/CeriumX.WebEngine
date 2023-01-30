87b77f9ded158691aa236e3794f7b0ac0df738f6
CockroachSoulful <cockroach888@outlook.com>
2023-01-30 16:52:35 +0800

    feat: 增加多个远程仓库æË½ű¾¡£

--- AutoPush.cmd
+++ AutoPush.cmd
@@ -11,8 +11,14 @@ git push gitee
 @echo\&echo 推送到 GitHub 远程仓库
 git push github
 
-@echo\&echo 推送到 Origin 远程仓库
-git push origin
+@echo\&echo 推送到 Bitbucket 远程仓库
+git push bitbucket
+
+@echo\&echo 推送到 Azure 远程仓库
+git push azure
+
+::@echo\&echo 推送到 Origin 远程仓库
+::git push origin
 
 @echo\&echo 查看本地仓库状态
 git status
